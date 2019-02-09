496038eaceca309f40d38cba739ddac2ab3ef2d9
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-02-09 17:26:56 +0530

    u

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
         return (
             <nav className="navbar navbar-expand-md navbar-dark" style={navbarStyle}>
                 <div className="container">
-                    <Link style={headerStyle} className="navbar-brand" to="/">NutriBudd</Link>
+                    <Link style={headerStyle} className="navbar-brand" to="/">NutriBuddy</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
