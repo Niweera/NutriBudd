bb17703298b2163c5e0b9a817bc28ccc64d04b08
HarindaVithana <harindavithana@gmail.com>
2019-02-14 11:55:50 +0530

    Added ingredients

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import { UserIsAuthenticated, UserIsNotAuthenticated} from './helpers/auth';
-import Header from './components/layout/Header';
-import Dashboard from './components/items/Dashboard';
-import HomePage from './components/items/HomePage';
-import { Provider } from 'react-redux';
-import NotFound from './components/pages/NotFound';
-import store from './store';
-import Login from './components/auth/Login';
-import Footer from './components/layout/Footer';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { UserIsAuthenticated, UserIsNotAuthenticated } from "./helpers/auth";
+import Header from "./components/layout/Header";
+import Dashboard from "./components/items/Dashboard";
+import HomePage from "./components/items/HomePage";
+import { Provider } from "react-redux";
+import NotFound from "./components/pages/NotFound";
+import store from "./store";
+import Login from "./components/auth/Login";
+import Footer from "./components/layout/Footer";
+import ingredients from "./components/pages/ingredients";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -19,16 +20,25 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <Header/>
+            <Header />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route exact path="/dashboard" component={UserIsAuthenticated(Dashboard)}/>
-                <Route exact path="/login" component={UserIsNotAuthenticated(Login)}/>
-                <Route component={NotFound}/>
+                <Route exact path="/" component={HomePage} />
+                <Route
+                  exact
+                  path="/dashboard"
+                  component={UserIsAuthenticated(Dashboard)}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  component={UserIsNotAuthenticated(Login)}
+                />
+                <Route exact path="/ingredients" component={ingredients} />
+                <Route component={NotFound} />
               </Switch>
             </div>
-            <Footer/>
+            <Footer />
           </div>
         </Router>
       </Provider>
---
+++ src/components/pages/ingredients.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ingredients extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <h1>Check ingredients</h1>
+      </div>
+    );
+  }
+}
+
+export default ingredients;
